9f058ecb6fea495b89c81964ad2256ffd5848e73
Dmitrii <cunaik@mail.ru>
2023-06-18 15:11:05 +0300

    сделал 1 метод

--- src/main/java/ru/skypro/hw2/course4/hw2course4/controller/EmployeeController.java
+++ src/main/java/ru/skypro/hw2/course4/hw2course4/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package ru.skypro.hw2.course4.hw2course4.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.hw2.course4.hw2course4.dto.EmployeeDTO;
 import ru.skypro.hw2.course4.hw2course4.projections.EmployeeFullInfo;
 import ru.skypro.hw2.course4.hw2course4.model.Position;
@@ -80,6 +82,12 @@ public class EmployeeController {
     public List<EmployeeDTO> getEmployeesOnPosition
             (@RequestParam(value = "position", required = false) String positionName) {
         return employeeService.getEmployeesOnPosition(positionName);
+    }
+
+    @PostMapping(value = "/upload" , consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void uploadFile(@RequestParam("file") MultipartFile file) {
+
+        employeeService.uploadEmployee(file);
     }
 
 }
