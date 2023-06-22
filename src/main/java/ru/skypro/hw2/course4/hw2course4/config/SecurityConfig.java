82991ff4b46a54ee56b1303cf0d2e3bf27fb53a4
DmitriiGienko <cuanik@mail.ru>
2023-06-22 21:11:54 +0300

    в процессе

--- src/main/java/ru/skypro/hw2/course4/hw2course4/config/SecurityConfig.java
+++ src/main/java/ru/skypro/hw2/course4/hw2course4/config/SecurityConfig.java
@@ -2,13 +2,17 @@ package ru.skypro.hw2.course4.hw2course4.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -45,5 +49,44 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeHttpRequests(this::customizeRequest);
+        // Цепочка фильтров безопасности для обработки входящих запросов,
+        // основанная на настройках безопасности,
+        // определенных с помощью метода 'customizeRequest'.
+
+        return http.build();
+    }
+
+    private void customizeRequest(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        try {
+            registry.requestMatchers(new AntPathRequestMatcher("/admin/**"))
+                    .hasAnyRole("ADMIN")
+                    // Определяем правило авторизации для запросов
+                    // к URL, которые начинаются с "/admin/",
+                    // позволяя доступ только пользователям с ролью "ADMIN".
+
+                    .requestMatchers(new AntPathRequestMatcher("/**"))
+                    .hasAnyRole("USER")
+                    // Определяем правило авторизации для остальных запросов,
+                    // позволяя доступ только пользователям с ролью "USER".
+
+                    .and()
+                    .formLogin().permitAll()
+                    // Позволяем всем пользователям доступ к форме входа.
+
+                    .and()
+                    .logout().logoutUrl("/logout");
+            // Настраиваем механизм выхода из системы
+            // с заданием URL "/logout".
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/ru/skypro/hw2/course4/hw2course4/controller/AdminEmployeeController.java
@@ -0,0 +1,99 @@
+package ru.skypro.hw2.course4.hw2course4.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.hw2.course4.hw2course4.dto.EmployeeDTO;
+import ru.skypro.hw2.course4.hw2course4.exceptions.IdNotFoundExceptions;
+import ru.skypro.hw2.course4.hw2course4.model.Position;
+import ru.skypro.hw2.course4.hw2course4.projections.EmployeeFullInfo;
+import ru.skypro.hw2.course4.hw2course4.service.EmployeeServiceImpl;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/employees")
+
+
+public class AdminEmployeeController {
+
+    private final EmployeeServiceImpl employeeService;
+
+    public AdminEmployeeController(EmployeeServiceImpl employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/showAllEmployees")
+    public List<EmployeeDTO> getAllEmployee() {
+        return employeeService.getAllEmployees();
+    }
+
+    @PostMapping("/addEmployees")
+    public void addEmployee(@RequestParam(required = false) String name,
+                            @RequestParam(required = false) int salary,
+                            @RequestParam Position position) {
+        employeeService.addEmployee(new EmployeeDTO(name, salary, position));
+    }
+
+    @PutMapping("/employees/{id}")
+    public void updateEmployee(@RequestParam int id,
+                               @RequestParam String newName,
+                               @RequestParam int newSalary) {
+        employeeService.updateEmployeeById(id, newName, newSalary);
+
+    }
+
+    @DeleteMapping("/deleteEmployees/{id}")
+    public void deleteEmployee(@RequestParam int id) {
+        employeeService.deleteEmployeeById(id);
+    }
+
+    @GetMapping("/employeesById/{id}")
+    public EmployeeDTO getEmployeeById(@PathVariable int id) {
+        return employeeService.getInfoEmployeeById(id);
+    }
+
+    @GetMapping("/salaryHigherThan")
+    public List<EmployeeDTO> getEmployeeWithSalaryAboveAverage(@RequestParam int compareSalary) {
+        return employeeService.getEmployeeWithSalaryMoreThan(compareSalary);
+    }
+
+    @GetMapping("/withHighestSalary")
+    public List<EmployeeDTO> getEmployeeWithMaxSalary() {
+        return employeeService.getEmployeeWithMaxSalary();
+    }
+
+    @GetMapping("/fullInfo")
+    public List<EmployeeFullInfo> getEmployeeFullInformation() {
+        return employeeService.getEmployeeFullInfo();
+    }
+
+    @GetMapping("/{id}/fullInfo")
+    public EmployeeDTO getEmployeeInfoById(@RequestParam int id) {
+        return employeeService.getFullInfoById(id);
+    }
+
+    @GetMapping("/EmployeesPages")
+    public List<EmployeeDTO> getEmployeesForPosition
+            (@RequestParam(required = false, defaultValue = "0") int page) {
+        return employeeService.getEmployeeByPage(page);
+    }
+
+    @GetMapping("/employees/position")
+    public List<EmployeeDTO> getEmployeesOnPosition
+            (@RequestParam(value = "position", required = false) String positionName) {
+        return employeeService.getEmployeesOnPosition(positionName);
+    }
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void uploadFile(@RequestParam("file") MultipartFile file) {
+
+        try {
+            employeeService.uploadEmployee(file);
+        } catch (IOException e) {
+            throw new IdNotFoundExceptions();
+        }
+    }
+
+}
